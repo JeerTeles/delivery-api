aee4aa1c57e3fc349881956bad763281f69d14c9
JeerTeles <jadersoncontatos@gmail.com>
2025-11-22 01:34:15 -0300

    feat/import @Repository

--- src/main/java/com/delivery_api/Projeto/delivery/API/repository/ClienteRepository.java
+++ src/main/java/com/delivery_api/Projeto/delivery/API/repository/ClienteRepository.java
@@ -1,4 +1,10 @@
 package com.delivery_api.Projeto.delivery.API.repository;
 
-public class ClienteRepository {
+import com.delivery_api.Projeto.delivery.API.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface  ClienteRepository extends JpaRepository<Cliente, Long> {
+
 }
