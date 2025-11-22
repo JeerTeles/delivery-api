c98449ff34413bf2cbf52b4a81e9fda4db70e04b
JeerTeles <jadersoncontatos@gmail.com>
2025-11-22 02:32:13 -0300

    feat/register customer

--- src/main/java/com/delivery_api/Projeto/delivery/API/controller/ClienteController.java
+++ src/main/java/com/delivery_api/Projeto/delivery/API/controller/ClienteController.java
@@ -1,7 +1,37 @@
 package com.delivery_api.Projeto.delivery.API.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.delivery_api.Projeto.delivery.API.entity.Cliente;
+import com.delivery_api.Projeto.delivery.API.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/clientes")
+@CrossOrigin(origins = "*")
 public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;
+
+    /**
+     * Cadastrar novo cliente
+     */
+    @PostMapping
+    public ResponseEntity<?> cadastrar(@Validated @RequestBody Cliente cliente) {
+        try {
+            Cliente clienteSalvo = clienteService.cadastrar(cliente);
+            return ResponseEntity.status(HttpStatus.CREATED).body(clienteSalvo);
+        } catch (IllegalAccessException e) {
+            return ResponseEntity.badRequest().body("Erro:" + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro interno do servidor");
+        }
+    }
+
+    // listar todos os clientes salvos
+
+
 }
