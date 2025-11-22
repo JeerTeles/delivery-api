cf519559c491b70d44d89884ffc98fb5c13acd63
JeerTeles <jadersoncontatos@gmail.com>
2025-11-22 00:20:16 -0300

    disabling login for development environment

--- src/main/java/com/delivery_api/Projeto/delivery/API/ProjetoDeliveryApiApplication.java
+++ src/main/java/com/delivery_api/Projeto/delivery/API/ProjetoDeliveryApiApplication.java
@@ -2,8 +2,11 @@ package com.delivery_api.Projeto.delivery.API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ProjetoDeliveryApiApplication {
 
 	public static void main(String[] args) {
