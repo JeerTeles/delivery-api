7193be92b3ebbf8b800075a578d7f4fd0e3529e7
JeerTeles <jadersoncontatos@gmail.com>
2025-11-26 02:06:14 -0300

    feat/implemented services

--- src/main/java/com/delivery_api/Projeto/delivery/API/entity/Cliente.java
+++ src/main/java/com/delivery_api/Projeto/delivery/API/entity/Cliente.java
@@ -1,8 +1,35 @@
 package com.delivery_api.Projeto.delivery.API.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cleintes")
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @Column(nullable = true)
+    private Boolean ativo;
+
+    public void inativar() {
+        this.ativo = false;
+    }
+
 
 }
