7c0d57e1643562946174a9d311b2f642197a0a76
JeerTeles <jadersoncontatos@gmail.com>
2025-11-26 02:07:44 -0300

    feat/read only

--- src/main/java/com/delivery_api/Projeto/delivery/API/repository/ClienteRepository.java
+++ src/main/java/com/delivery_api/Projeto/delivery/API/repository/ClienteRepository.java
@@ -4,7 +4,22 @@ import com.delivery_api.Projeto.delivery.API.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface  ClienteRepository extends JpaRepository<Cliente, Long> {
 
+    // Buscar por email (método derivado)
+
+    Optional<Cliente> findByEmail(String email);
+
+    //Verificar se email já existe
+    boolean existsByEmail(String email);
+
+    //Buscar cleintes ativos
+    List<Cliente> findByAtivoTrue();
+
+    // Buscar cleintes por nome (contendo)
+    List<Cliente> findByAtivoContaining(String nome);
 }
--- src/main/java/com/delivery_api/Projeto/delivery/API/service/ClienteService.java
+++ src/main/java/com/delivery_api/Projeto/delivery/API/service/ClienteService.java
@@ -1,9 +1,54 @@
 package com.delivery_api.Projeto.delivery.API.service;
 
+import com.delivery_api.Projeto.delivery.API.entity.Cliente;
+import com.delivery_api.Projeto.delivery.API.repository.ClienteRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@Transactional
 public class ClienteService {
+    @Autowired
+    private ClienteRepository clienteRepository;
 
+    @Transactional(readOnly = true)
+    public Cliente cadastrar(Cliente cliente) {
+        Cliente cliente1 = new Cliente();
+        return cliente1;
+    }
 
+    @Transactional(readOnlY = true)
+    public List<Cliente> listarAtivos() {
+        return new ArrayList<Cliente>();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    @Transactional(readOnle = true)
+    public Cliente atualizar(Long id, Cliente cliente) {
+        return  new Cliente();
+    }
+
+    @Transactional(readOnlY = true)
+    public void inativar(Long id) {
+
+    }
+
+    @Transactional(readOnlY = true)
+    public List<Cliente> buscarPorNome(String nome) {
+        return new ArrayList<>();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Cliente> buscarPorEmail(String email) {
+        return clienteRepository.findByEmail(email);
+    }
 }
